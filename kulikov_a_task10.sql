1f1794db1ea94089dc4463624cd210cce97aa94c
Alexander <ferfei12126@gmail.com>
2024-10-04 11:24:32 +0400

    Изменение кодировки на UTF-8

--- kulikov_a_task10.sql
+++ kulikov_a_task10.sql
@@ -1,19 +1,19 @@
---Решение. Задание 10. Куликов А.А.
+--Р РµС€РµРЅРёРµ. Р—Р°РґР°РЅРёРµ 10. РљСѓР»РёРєРѕРІ Рђ.Рђ.
 
 /*
-Автор: Куликов А.А.
-Описание: API для сущностей "Платеж" и "Детали платежа"
+РђРІС‚РѕСЂ: РљСѓР»РёРєРѕРІ Рђ.Рђ.
+РћРїРёСЃР°РЅРёРµ: API РґР»СЏ СЃСѓС‰РЅРѕСЃС‚РµР№ "РџР»Р°С‚РµР¶" Рё "Р”РµС‚Р°Р»Рё РїР»Р°С‚РµР¶Р°"
 */
 
---Создание платежа
+--РЎРѕР·РґР°РЅРёРµ РїР»Р°С‚РµР¶Р°
 declare
-  v_message varchar2(100 char) := 'Платеж создан';
+  v_message varchar2(100 char) := 'РџР»Р°С‚РµР¶ СЃРѕР·РґР°РЅ';
   c_create constant payment.status%type := 0;
   v_current_dtime date := sysdate;
   v_payment_id payment.payment_id%type;
   v_payment_detail t_payment_detail_array := t_payment_detail_array(t_payment_detail(1,'internal terminal')
                                                                    ,t_payment_detail(2,'199.5.49.657')
-                                                                   ,t_payment_detail(3,'пополнение через терминал')                                                                  
+                                                                   ,t_payment_detail(3,'РїРѕРїРѕР»РЅРµРЅРёРµ С‡РµСЂРµР· С‚РµСЂРјРёРЅР°Р»')                                                                  
                                                                    );
 begin
   
@@ -22,86 +22,86 @@ begin
     for i in v_payment_detail.first .. v_payment_detail.last loop
       
       if v_payment_detail(i).field_id is null then
-        dbms_output.put_line('ID поля не может быть пустым');
+        dbms_output.put_line('ID РїРѕР»СЏ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
       end if;
       
       if v_payment_detail(i).field_value is null then
-        dbms_output.put_line('Значение в поле не может быть пустым');
+        dbms_output.put_line('Р—РЅР°С‡РµРЅРёРµ РІ РїРѕР»Рµ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
       end if;
       
       dbms_output.put_line('Field_id: ' || v_payment_detail(i).field_id || '. Value: ' || v_payment_detail(i).field_value);
     end loop;
   
   else    
-    dbms_output.put_line('Коллекция не содержит данных');  
+    dbms_output.put_line('РљРѕР»Р»РµРєС†РёСЏ РЅРµ СЃРѕРґРµСЂР¶РёС‚ РґР°РЅРЅС‹С…');  
   end if;
 
-  dbms_output.put_line(v_message || '. Статус: ' || c_create);
+  dbms_output.put_line(v_message || '. РЎС‚Р°С‚СѓСЃ: ' || c_create);
   dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24:mi:ss'));
 end;
 /
 
---Сброс платежа
+--РЎР±СЂРѕСЃ РїР»Р°С‚РµР¶Р°
 declare
-  v_message varchar2(200 char) := 'Сброс платежа в "ошибочный статус" с указанием причины';
+  v_message varchar2(200 char) := 'РЎР±СЂРѕСЃ РїР»Р°С‚РµР¶Р° РІ "РѕС€РёР±РѕС‡РЅС‹Р№ СЃС‚Р°С‚СѓСЃ" СЃ СѓРєР°Р·Р°РЅРёРµРј РїСЂРёС‡РёРЅС‹';
   c_error constant payment.status%type := 2;
-  v_reason payment.status_change_reason%type := 'недостаточно средств';
+  v_reason payment.status_change_reason%type := 'РЅРµРґРѕСЃС‚Р°С‚РѕС‡РЅРѕ СЃСЂРµРґСЃС‚РІ';
   v_current_dtime timestamp := systimestamp;
   v_payment_id payment.payment_id%type;
 begin
   if v_payment_id is null then 
-    dbms_output.put_line('ID платежа не может быть пустым');
+    dbms_output.put_line('ID РїР»Р°С‚РµР¶Р° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
   end if;
   
   if v_reason is null then
-    dbms_output.put_line('Причина не может быть пустой');
+    dbms_output.put_line('РџСЂРёС‡РёРЅР° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚РѕР№');
   end if;
   
-  dbms_output.put_line(v_message || '. Статус: ' || c_error || '. Причина: ' || v_reason);
+  dbms_output.put_line(v_message || '. РЎС‚Р°С‚СѓСЃ: ' || c_error || '. РџСЂРёС‡РёРЅР°: ' || v_reason);
   dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24:mi:ss:ff'));
 end;
 /
 
---Отмена платежа
+--РћС‚РјРµРЅР° РїР»Р°С‚РµР¶Р°
 declare
-  v_message varchar2(200 char) := 'Отмена платежа с указанием причины';
+  v_message varchar2(200 char) := 'РћС‚РјРµРЅР° РїР»Р°С‚РµР¶Р° СЃ СѓРєР°Р·Р°РЅРёРµРј РїСЂРёС‡РёРЅС‹';
   c_cancel constant payment.status%type := 3;
-  v_reason payment.status_change_reason%type := 'ошибка пользователя';
+  v_reason payment.status_change_reason%type := 'РѕС€РёР±РєР° РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ';
   v_current_dtime date := sysdate;
   v_payment_id payment.payment_id%type := 15;
 begin
   if v_payment_id is null then 
-    dbms_output.put_line('ID платежа не может быть пустым');
+    dbms_output.put_line('ID РїР»Р°С‚РµР¶Р° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
   end if;
   
   if v_reason is null then
-    dbms_output.put_line('Причина не может быть пустой');
+    dbms_output.put_line('РџСЂРёС‡РёРЅР° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚РѕР№');
   end if;
   
-  dbms_output.put_line(v_message || '. Статус: '|| c_cancel ||'. Причина: ' || v_reason);
+  dbms_output.put_line(v_message || '. РЎС‚Р°С‚СѓСЃ: '|| c_cancel ||'. РџСЂРёС‡РёРЅР°: ' || v_reason);
   dbms_output.put_line(to_char(v_current_dtime,'dd-mm-yyyy'));
 end;
 /
 
---Успешный платеж
+--РЈСЃРїРµС€РЅС‹Р№ РїР»Р°С‚РµР¶
 declare
-  v_message varchar2(200 char) := 'Успешное завершение платежа';
+  v_message varchar2(200 char) := 'РЈСЃРїРµС€РЅРѕРµ Р·Р°РІРµСЂС€РµРЅРёРµ РїР»Р°С‚РµР¶Р°';
   c_success constant payment.status%type := 1;
   v_current_dtime timestamp := systimestamp;
   v_payment_id payment.payment_id%type;
 begin
   if v_payment_id is null then 
-    dbms_output.put_line('ID платежа не может быть пустым');
+    dbms_output.put_line('ID РїР»Р°С‚РµР¶Р° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
   end if;
   
-  dbms_output.put_line(v_message || '. Статус: ' || c_success);
+  dbms_output.put_line(v_message || '. РЎС‚Р°С‚СѓСЃ: ' || c_success);
   dbms_output.put_line(to_char(v_current_dtime,'dd Month yyyy hh24:mi'));
 end;
 /
 
---Добавление/изменение данных по платежу
+--Р”РѕР±Р°РІР»РµРЅРёРµ/РёР·РјРµРЅРµРЅРёРµ РґР°РЅРЅС‹С… РїРѕ РїР»Р°С‚РµР¶Сѓ
 declare
-  v_message varchar2(200 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
+  v_message varchar2(200 char) := 'Р”Р°РЅРЅС‹Рµ РїР»Р°С‚РµР¶Р° РґРѕР±Р°РІР»РµРЅС‹ РёР»Рё РѕР±РЅРѕРІР»РµРЅС‹ РїРѕ СЃРїРёСЃРєСѓ id_РїРѕР»СЏ/Р·РЅР°С‡РµРЅРёРµ';
   v_current_dtime date := sysdate;
   v_payment_id payment.payment_id%type := 120;
   v_payment_detail t_payment_detail_array := t_payment_detail_array(t_payment_detail(2,'199.6.94.888')                                                                
@@ -113,22 +113,22 @@ begin
     for i in v_payment_detail.first .. v_payment_detail.last loop
       
       if v_payment_detail(i).field_id is null then
-        dbms_output.put_line('ID поля не может быть пустым');
+        dbms_output.put_line('ID РїРѕР»СЏ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
       end if;
       
       if v_payment_detail(i).field_value is null then
-        dbms_output.put_line('Значение в поле не может быть пустым');
+        dbms_output.put_line('Р—РЅР°С‡РµРЅРёРµ РІ РїРѕР»Рµ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
       end if;
       
       dbms_output.put_line('Field_id: ' || v_payment_detail(i).field_id || '. Value: ' || v_payment_detail(i).field_value);
     end loop;
   
   else    
-    dbms_output.put_line('Коллекция не содержит данных');  
+    dbms_output.put_line('РљРѕР»Р»РµРєС†РёСЏ РЅРµ СЃРѕРґРµСЂР¶РёС‚ РґР°РЅРЅС‹С…');  
   end if;
   
   if v_payment_id is null then 
-    dbms_output.put_line('ID платежа не может быть пустым');
+    dbms_output.put_line('ID РїР»Р°С‚РµР¶Р° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
   end if;
   
   dbms_output.put_line(v_message);
@@ -136,24 +136,24 @@ begin
 end;
 /
 
---Удаление данных по деталям платежа
+--РЈРґР°Р»РµРЅРёРµ РґР°РЅРЅС‹С… РїРѕ РґРµС‚Р°Р»СЏРј РїР»Р°С‚РµР¶Р°
 declare
-  v_message varchar2(200 char) := 'Детали платежа удалены по списку id_полей';
+  v_message varchar2(200 char) := 'Р”РµС‚Р°Р»Рё РїР»Р°С‚РµР¶Р° СѓРґР°Р»РµРЅС‹ РїРѕ СЃРїРёСЃРєСѓ id_РїРѕР»РµР№';
   v_current_dtime timestamp := systimestamp;
   v_payment_id payment.payment_id%type;
   v_delete_field_ids t_number_array := t_number_array(1, 2);
 begin
   
   if v_payment_id is null then 
-    dbms_output.put_line('ID платежа не может быть пустым');
+    dbms_output.put_line('ID РїР»Р°С‚РµР¶Р° РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹Рј');
   end if;
   
   if v_delete_field_ids is empty then
-    dbms_output.put_line('Коллекция не содержит данных');
+    dbms_output.put_line('РљРѕР»Р»РµРєС†РёСЏ РЅРµ СЃРѕРґРµСЂР¶РёС‚ РґР°РЅРЅС‹С…');
   end if;
   
   dbms_output.put_line(v_message);
   dbms_output.put_line(to_char(v_current_dtime,'"Date: " dd Month yyyy ". Time: " hh24:mi:ss:ff'));
-  dbms_output.put_line('Количество полей для удаления: ' || v_delete_field_ids.count());
+  dbms_output.put_line('РљРѕР»РёС‡РµСЃС‚РІРѕ РїРѕР»РµР№ РґР»СЏ СѓРґР°Р»РµРЅРёСЏ: ' || v_delete_field_ids.count());
 end;
 /
